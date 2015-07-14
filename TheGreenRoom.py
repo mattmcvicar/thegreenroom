a2b72d517db83d5a707cfc1a10a1e54f59b8b241
mattmcvicar <mattmcvicar@hotmail.com>
2015-07-14 15:40:06 +0100

    registered the app

---
+++ TheGreenRoom.py
@@ -0,0 +1,6 @@
+# Parameters -----------------------------------
+CLIENT_ID =	"58e08193e2f147b7a3f509964870894c"
+CLIENT_SECRET = "14a8f2af907549bc88316c829c59c073"
+
+# Main ------------------------------------------
+print "My app is registered"
